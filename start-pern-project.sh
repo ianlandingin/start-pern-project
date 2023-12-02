a6f893543f1e2b3543e6aacd9b451297a27ef034
ianlandingin <iclandingin28@gmail.com>
2023-12-02 10:03:13 +0800

    added loops to the initial questions

--- start-pern-project.sh
+++ start-pern-project.sh
@@ -7,7 +7,7 @@ next_line (){
 
 echo "In what environment are you running this script?
 (1 - Windows; 2 - Linux/WSL(Suggested);): "
-read -r OS
+read -rn 1 OS
 next_line
 if [ $OS = 1 ]; then
   clear
@@ -36,7 +36,7 @@ But before we do, make sure you have installed the following:
 - Postgresql 15 or later.
 - and, Latest version of Node.js
 Do you have them installed?(Y/n): "
-read -r INSTALLED
+read -rn 1 INSTALLED
 
 echo "$INSTALLED"
 
@@ -60,17 +60,98 @@ else
 fi
 done
 
+PROJECT=0
+while [ $PROJECT -eq 0 ]
+do
+  read -rp "What should we name the project? (ex. no-spaces, No-Spaces): " PROJECT_NAME
+  # Remove any whitespace
+  PROJECT_NAME="${PROJECT_NAME// }"
 
-echo "New database name?: "
-read -r DB_NAME
+  if [ ! "${PROJECT_NAME,,}" ]; then
+  clear
+  echo "Please put a project name"
+  next_line
+  else
+    echo "Project name: $PROJECT_NAME"
+    ((PROJECT++))
+  fi
+done
 
-echo "New psql user for database?: "
-read -r DB_USER
+DATABASE=0
+while [ $DATABASE -eq 0 ]
+do
+  read -rp "New Database name?: " DATABASE_NAME
+  # Remove any whitespace
+  DATABASE_NAME="${DATABASE_NAME// }"
 
-echo "Password: "
-read -rs PASSWORD
-echo "Repeat password: "
-read -rs RE_PASSWORD
+  if [ ! "${DATABASE_NAME,,}" ]; then
+  echo "Please put your database name"
+  else
+    echo "Database name: $DATABASE_NAME"
+    ((DATABASE++))
+  fi
+done
 
-echo "Do you need multiple tables for this project?(Y/n): "
+USER=0
+while [ $USER -eq 0 ]
+do
+  read -rp "New psql user for database?: " DATABASE_USER
+  # Remove any whitespace
+  DATABASE_USER="${DATABASE_USER// }"
+
+  if [ ! "${DATABASE_USER,,}" ]; then
+  echo "Please put your username"
+  else
+    echo "Username name: $DATABASE_USER"
+    ((USER++))
+  fi
+done
+
+PASS_MATCH=0
+while [ $PASS_MATCH -eq 0 ]
+do
+
+  PASS=0
+  while [ $PASS -eq 0 ]
+  do
+    read -rsp "Password: " PASSWORD
+    # Remove any whitespace
+    PASSWORD="${PASSWORD// }"
+
+    if [ ! "${PASSWORD,,}" ]; then
+    echo "Please put your password"
+    else
+      next_line
+      ((PASS++))
+    fi
+  done
+
+  RE_PASS=0
+  while [ $RE_PASS -eq 0 ]
+  do
+    read -rsp "Repeat password: " RE_PASSWORD
+    # Remove any whitespace
+    RE_PASSWORD="${RE_PASSWORD// }"
+
+    if [ ! "${RE_PASSWORD,,}" ]; then
+    echo "Please put your password"
+    else
+      next_line
+      ((RE_PASS++))
+    fi
+  done
+
+  if [ "${PASSWORD,,}" == "${RE_PASSWORD,,}" ]
+  then
+    ((PASS_MATCH++))
+  else
+    next_line
+    echo "Passwords doesn't match."
+  fi
+done
+
+
+
+# echo "Do you need multiple tables for this project?(Y/n): "
+
 
